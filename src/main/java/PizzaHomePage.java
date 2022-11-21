0b31ad7f8111563911f2b6a1ddbc360871dcc2b0
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-11-21 16:19:25 +0100

    Add Drink tests

--- src/main/java/PizzaHomePage.java
+++ src/main/java/PizzaHomePage.java
@@ -5,11 +5,13 @@ public class PizzaHomePage extends Page {
     protected static String pizzaSiteUrl = "https://dominos.by/";
     private final By closeAdvertisementButton = By.xpath("//button[@class='custom-button custom-button--transparent custom-button--no-paddings custom-button--size-small custom-modal__close-button']");
     private final By closeCookieButton = By.xpath("//div[@class='sm-notify-accept']");
-    private final By fullBasketIcon = By.xpath("//span[@class='jss133 cart-icon__badge']");
+    private final By fullBasketIcon = By.xpath("//span[@class='jss158']");
     private final By pizzaSection = By.xpath("//li[@class='horizontal-menu__list-item'][1]/a");
+    private final By drinkSection = By.xpath("//span[text()='Напитки']");
+    private final By addDrinkToBasket = By.xpath("//div[text()='Чай Клюква с имбирем']");
     private final By diameterValue = By.xpath("//div[text()='4 Сезона']/../div[@class='modifications']/div[@class='modifications__content']/div[1]");
     private final By enormousPizzaSize = By.xpath("//div[text()='4 Сезона']/../div[@class='modifications']/div[@class='modifications__content']/div[1]/div/div/div/select/option[2]");
-    private final By addToBasket = By.xpath("//div[text()='4 Сезона']/../div[@class='product-card__modification-summary']/div[2]");
+    private final By addPizzaToBasket = By.xpath("//div[text()='4 Сезона']/../div[@class='product-card__modification-summary']/div[2]");
     private final By basket = By.xpath("//div[@data-test='cart-button']");
     private final By submitOrderButton = By.xpath("//div[contains(@class,'cart-button__mini-bag-actions cart-button__mini-bag-actions--bottom')]");
 
@@ -32,18 +34,34 @@ public class PizzaHomePage extends Page {
         driver.findElement(pizzaSection).click();
         driver.findElement(diameterValue).click();
         driver.findElement(enormousPizzaSize).click();
-        driver.findElement(addToBasket).click();
+        driver.findElement(addPizzaToBasket).click();
         return new PizzaHomePage(driver);
     }
 
-    public BasketPage submitOrder() {
+    public PizzaHomePage addDrinkToBasket() {
+        driver.findElement(drinkSection).click();
+        driver.findElement(closeCookieButton).click();
+        driver.findElement(addDrinkToBasket).click();
+        return new PizzaHomePage(driver);
+    }
+
+    public BasketPage submitPizzaOrder() {
         addPizzaToBasket();
         driver.findElement(basket).click();
         driver.findElement(closeCookieButton).click();
         driver.findElement(submitOrderButton).click();
         return new BasketPage(driver);
     }
-    public boolean isBasketIconPresent(){
+
+    public BasketPage submitDrinkOrder() {
+        addDrinkToBasket();
+        driver.findElement(basket).click();
+        driver.findElement(closeCookieButton).click();
+        driver.findElement(submitOrderButton).click();
+        return new BasketPage(driver);
+    }
+
+    public boolean isProductAddedToBasket() {
         return !driver.findElements(fullBasketIcon).isEmpty();
 
     }
